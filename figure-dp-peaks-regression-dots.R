6cb276e62f1481e9f50d0e8fc3ebd53d28de1ad4
Toby Dylan Hocking <tdhock5@gmail.com>
2015-01-16 10:00:03 -0500

    L1.reg test error fig

--- figure-dp-peaks-regression-dots.R
+++ figure-dp-peaks-regression-dots.R
@@ -1,21 +1,33 @@
-works_with_R("3.1.1", reshape2="1.2.2", ggplot2="1.0", dplyr="0.2",
+works_with_R("3.1.2", reshape2="1.2.2", ggplot2="1.0",
+             "Rdatatable/data.table@fd5464e4c587d4a96fa32c809eb94a45bb361133",
+             dplyr="0.4.0",
              xtable="1.7.3")
 
 load("dp.peaks.baseline.RData")
 load("dp.peaks.regression.RData")
+load("regularized.all.RData")
 
-regression.set.i <- dp.peaks.regression %.%
-  group_by(set.name, set.i) %.%
+reg <- regularized.all$error %>%
+  group_by(set.name, set.i, model.name) %>%
   summarise(errors=sum(errors),
-            regions=sum(regions)) %.%
+            regions=sum(regions)) %>%
+  mutate(percent=errors/regions*100,
+         algorithm=model.name)
+
+regression.set.i <- dp.peaks.regression %>%
+  group_by(set.name, set.i) %>%
+  summarise(errors=sum(errors),
+            regions=sum(regions)) %>%
   mutate(algorithm="PeakSeg")
 both.cols <- c("set.name", "set.i", "algorithm", "errors", "regions")
 
-both <- rbind(regression.set.i[, both.cols], dp.peaks.baseline[, both.cols]) %.%
-  group_by() %.%
+both <- rbind(regression.set.i[, both.cols],
+              dp.peaks.baseline[, both.cols],
+              data.frame(reg)[, both.cols]) %>%
+  group_by() %>%
   mutate(percent=errors/regions*100,
          set.name=as.character(set.name))
-         
+
 wide <- dcast(both, set.name + set.i ~ algorithm, value.var="errors")
 
 competitors <-
@@ -49,6 +61,18 @@ ggplot()+
 algo.colors <-
   c(macs.trained="#1B9E77", PeakSeg="#D95F02", hmcan.broad.trained="#7570B3")
 both$algo <- sub(".trained", "", both$algorithm)
+
+mean.both <- both %>%
+  group_by(set.name, algo) %>%
+  summarise(mean=mean(percent))
+algo.stats <- mean.both %>%
+  group_by(algo) %>%
+  summarise(mean=mean(mean)) %>%
+  arrange(mean)
+algo.levs <- rev(algo.stats$algo)
+both$algo <- factor(both$algo, algo.levs)
+mean.both$algo <- factor(mean.both$algo, algo.levs)
+
 dp.peaks.regression.dots <- both
 save(dp.peaks.regression.dots, file="dp.peaks.regression.dots.RData")
 boxes <- 
@@ -84,6 +108,8 @@ dev.off()
 
 dots <- 
 ggplot()+
+  geom_point(aes(algo, mean),
+             data=mean.both, color="grey", size=5)+
   geom_point(aes(algo, percent),
              data=both, pch=1)+
   facet_grid(.~set.name, labeller=function(var, val){
@@ -94,6 +120,6 @@ ggplot()+
   theme(panel.margin=grid::unit(0, "cm"))+
   coord_flip()+
   scale_y_continuous("percent test error", breaks=seq(0, 50, by=25))
-pdf("figure-dp-peaks-regression-dots.pdf", h=1.6)
+pdf("figure-dp-peaks-regression-dots.pdf", h=3)
 print(dots)
 dev.off()
